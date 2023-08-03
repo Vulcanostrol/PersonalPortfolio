0422eef67898b2d86ffa8a346d3aa768361fbaca
Koen van den Brink <koen.brink.vd@gmail.com>
2023-08-03 17:53:55 +0200

    💬 Adds information about experience

--- src/pages/cv/experience.tsx
+++ src/pages/cv/experience.tsx
@@ -1,10 +1,10 @@
-import Accordion from "@/components/accordion";
+import Accordion from "@/components/Accordion";
 
 export default function WorkExperience() {
   return (
     <>
       <h1 className="mb-4 mt-16 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
-        💼 Work experience
+        💼 My experience
       </h1>
       <Accordion
         header={(
@@ -14,6 +14,22 @@ export default function WorkExperience() {
               <time dateTime="2023-06-01" title="June, 2023">June, 2023</time>
               &nbsp;-&nbsp;
               <time dateTime="2023-10-01" title="October, 2023">October, 2023</time>
+            </p>
+          </>
+        )}
+        body={(
+          <>
+            <p>
+              I worked with <a href="https://gecosistema.com/" className="link-base">GecoSistema</a> during my master thesis. My thesis
+              tackled hydrological level forecasting with <acronym title="Machine Learning">ML</acronym> in Italian
+              rivers.
+            </p>
+            <p className="mt-4">
+              After the <a href="https://en.wikipedia.org/wiki/2023_Emilia-Romagna_floods" className="link-base">Emilia-Romagna floods</a> in
+              2023, GecoSistema partnered with the European Union <a href="https://www.copernicus.eu/en" className="link-base">Copernicus</a>
+              project. I was involved with the project because of my master thesis, and I was responsible for building
+              a scalable system for hydrological forecasting in european rivers. This project will give european
+              citizens earlier warnings about flash floods, making evacuation more effective.
             </p>
           </>
         )}
@@ -64,6 +80,16 @@ export default function WorkExperience() {
               <time dateTime="2020-09-01" title="September, 2020">September, 2020</time>
               &nbsp;-&nbsp;
               <time dateTime="2022-09-01" title="September, 2022">September, 2022</time>
+            </p>
+          </>
+        )}
+        body={(
+          <>
+            <p>
+              At <a href="https://esportsteamtwente.nl/" className="link-base">Esports Team Twente</a>, I was
+              responsible for setting up the data pipeline for one of their teams' games. In addition to this, I did
+              some projects with <acronym title="Machine Learning">ML</acronym> to give the teams insight in their
+              communication and pathing efficiencies.
             </p>
           </>
         )}
